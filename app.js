3a7440e47dedcab6937bab0e5e74f1e79f369354
AnveshDasari <anvdasari@meta.com>
2026-05-27 12:44:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // import express  from "express"
 const express = require('express');
+const app = express()
+const port = process.env.PORT || 4000
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 // Create an Express app
 const app = express();
 
